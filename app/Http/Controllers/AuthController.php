a483f98577e2250f8692c9ffdd88e4a4884b0ab4
nadiaistikomatuzz <nadiazzz1231@gmail.com>
2020-08-08 20:34:04 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        echo 'Nis : 3103118098';
+        echo 'Name : Nadia Istikomatuz Z';
+        echo 'Gender : Female';
+        echo 'Phone : 083116295863';
+        echo 'Class : "XII RPL 3';
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+
+Route::get('/me', function (){
+    return ('me'); 
+});
+
+Route::get ('me', 'AuthController@index');
+
